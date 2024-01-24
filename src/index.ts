054b3ef51dbcd34a483f29b5e01114aefe77c71e
glaunay <pitooon@gmail.com>
2024-01-24 13:58:57 +0100

    publishing experimental

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { SocketRouter as Router} from './manager';
 export { ListenTo, SocketControllerRegister, SocketController} from './controllers'
+export { Socket } from 'socket.io';
 
 export default Router;
